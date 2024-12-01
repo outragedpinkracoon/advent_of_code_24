2cb6514dc0891d1f25ed810c50701b40699ccf37
Valerie Dryden <outragedpinkracoon@gmail.com>
2024-12-01 21:34:50 +0000

    more comments

--- day_one/part_one/lib/day_one_part_one.rb
+++ day_one/part_one/lib/day_one_part_one.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module DayOnePartOne
+  # takes in the path to the file and returns the sum of the differences
+  # between the numbers when they are ordered smallest to biggest in pairs.
   def self.run(file_path)
     set_one, set_two = parse(file_path)
     diffs = match(set_one, set_two)
@@ -10,10 +12,10 @@ module DayOnePartOne
   # returns a 'tuple' with the first set of numbers [1,3,2] and the second [4,4,5]
   # in this format [[1,3,2], [4,4,5]]
   def self.parse(file_path)
-    File.readlines(file_path).each_with_object([[], []]) do |item, obj|
-      parts = item.split
-      obj[0] << parts[0].to_i
-      obj[1] << parts[1].to_i
+    File.readlines(file_path).each_with_object([[], []]) do |item, tuple|
+      parts = item.split # e.g "3   6" becomes [3,6]
+      tuple[0] << parts[0].to_i
+      tuple[1] << parts[1].to_i
     end
   end
 
@@ -31,42 +33,3 @@ module DayOnePartOne
     diffs
   end
 end
-
-# class DayOnePartOne
-#   attr_reader :set_one, :set_two, :diffs, :sum
-
-#   def initialize(file_path)
-#     @file_path = file_path
-#     @set_one = []
-#     @set_two = []
-#     @diffs = []
-
-#     parse
-#     sort
-#     match
-#     @sum = diffs.sum
-#   end
-
-#   private
-
-#   attr_reader :file_path
-
-#   def parse
-#     File.foreach(file_path) do |line|
-#       parts = line.split
-#       set_one << parts[0].to_i
-#       set_two << parts[1].to_i
-#     end
-#   end
-
-#   def sort
-#     @set_one.sort!
-#     @set_two.sort!
-#   end
-
-#   def match
-#     @set_one.each_with_index do |item, index|
-#       @diffs << (item - @set_two[index]).abs
-#     end
-#   end
-# end
