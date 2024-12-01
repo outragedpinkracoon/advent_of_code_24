c356d5cc2ed1200d949fdaad8679e8242543aed9
Valerie Dryden <outragedpinkracoon@gmail.com>
2024-12-01 22:32:24 +0000

    final tidy

--- day_one/part_two/lib/day_one_part_two.rb
+++ day_one/part_two/lib/day_one_part_two.rb
@@ -7,15 +7,18 @@ module DayOne
       similarities(set_one, set_two).sum
     end
 
-    # Tallies the numbers in the second set e.g {4=>1, 3=>3, 5=>1, 9=>1} for
-    # [4, 3, 5, 3, 9, 3] then checks if the numbers in set one appear in this set
-    # if so, returns the multiple of the first set * the occurance of it e.g.
-    # first set = [3, 4, 2, 1, 3, 3] => 3 => 3 times in set 2 = 9
+    # Checks how many times the number from set one is in set two. Returns the
+    # number of times multiplied by the number itself.
+    # e.g. [3, 4, 2, 1, 3, 3] => 3 appears 3 times in set 2 = 9
     def self.similarities(set_one, set_two)
+      # makes a tally of the counts in the array e.g. {4=>1, 3=>3, 5=>1, 9=>1}
+      # for [4, 3, 5, 3, 9, 3]
       tallies = set_two.tally
 
+      # for each item in the first set, see how often it occurs in set two
       set_one.map do |item|
         count = tallies[item]
+        # if it appears, multiply it by the number of occurances
         count.nil? ? 0 : count * item
       end
     end
