42a891fe6d3b0061bb0b62c9d46f46b68f2d9b9f
Valerie Dryden <outragedpinkracoon@gmail.com>
2024-12-01 21:04:25 +0000

    finds diff between the sets

--- day_one/part_one/lib/day_one_part_one.rb
+++ day_one/part_one/lib/day_one_part_one.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 class DayOnePartOne
-  attr_reader :set_one, :set_two
+  attr_reader :set_one, :set_two, :diffs
 
   def initialize(file_path)
     @file_path = file_path
     @set_one = []
     @set_two = []
+    @diffs = []
 
     parse
     sort
+    match
   end
 
   private
@@ -27,5 +29,11 @@ class DayOnePartOne
   def sort
     @set_one.sort!
     @set_two.sort!
+  end
+
+  def match
+    @set_one.each_with_index do |item, index|
+      @diffs << (item - @set_two[index]).abs
+    end
   end
 end
--- day_one/part_one/test/day_one_part_one_test.rb
+++ day_one/part_one/test/day_one_part_one_test.rb
@@ -4,10 +4,18 @@ require_relative '../../../test_helper'
 require_relative '../lib/day_one_part_one'
 
 class DayOnePartOneTest < Minitest::Test
-  def test_can_parse_data
-    dopo = DayOnePartOne.new('day_one/part_one/data/test_data.txt')
+  def setup
+    @dopo = DayOnePartOne.new('day_one/part_one/data/test_data.txt')
+  end
 
-    assert_equal([1, 2, 3, 3, 3, 4], dopo.set_one)
-    assert_equal([3, 3, 3, 4, 5, 9], dopo.set_two)
+  def test_can_parse_data
+    assert_equal([1, 2, 3, 3, 3, 4], @dopo.set_one)
+    assert_equal([3, 3, 3, 4, 5, 9], @dopo.set_two)
+  end
+
+  def test_can_match_data
+    @dopo = DayOnePartOne.new('day_one/part_one/data/test_data.txt')
+
+    assert_equal([2, 1, 0, 1, 2, 5], @dopo.diffs)
   end
 end
