ba562a264d6a64fffe5327898a40937d38a15061
Valerie Dryden <outragedpinkracoon@gmail.com>
2024-12-01 20:13:03 +0000

    initial commit

---
+++ day 1/part 1/lib/main.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+module Day1Part1
+end
---
+++ day 1/part 1/test/main_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative '../../../test_helper'
+require_relative '../lib/main'
+
+class MainTest < Minitest::Test
+  def test_is_true
+    assert(true)
+  end
+end
