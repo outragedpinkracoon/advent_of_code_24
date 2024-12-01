dafd6cfc337afbbf161d1a40e5bfd93d52ff7178
Valerie Dryden <outragedpinkracoon@gmail.com>
2024-12-01 21:25:48 +0000

    converts to functional style

--- day_one/part_one/lib/day_one_part_one.rb
+++ day_one/part_one/lib/day_one_part_one.rb
@@ -1,40 +1,70 @@
 # frozen_string_literal: true
 
-class DayOnePartOne
-  attr_reader :set_one, :set_two, :diffs, :sum
-
-  def initialize(file_path)
-    @file_path = file_path
-    @set_one = []
-    @set_two = []
-    @diffs = []
-
-    parse
-    sort
-    match
-    @sum = diffs.sum
+module DayOnePartOne
+  def self.run(file_path)
+    set_one, set_two = parse(file_path)
+    diffs = match(set_one, set_two)
+    diffs.sum
   end
 
-  private
+  def self.parse(file_path)
+    set_one = []
+    set_two = []
 
-  attr_reader :file_path
-
-  def parse
     File.foreach(file_path) do |line|
       parts = line.split
       set_one << parts[0].to_i
       set_two << parts[1].to_i
     end
+    [set_one, set_two]
   end
 
-  def sort
-    @set_one.sort!
-    @set_two.sort!
-  end
-
-  def match
-    @set_one.each_with_index do |item, index|
-      @diffs << (item - @set_two[index]).abs
+  def self.match(set_one, set_two)
+    diffs = []
+    sorted_set_one = set_one.sort
+    sorted_set_two = set_two.sort
+    sorted_set_one.each_with_index do |item, index|
+      diffs << (item - sorted_set_two[index]).abs
     end
+    diffs
   end
 end
+
+# class DayOnePartOne
+#   attr_reader :set_one, :set_two, :diffs, :sum
+
+#   def initialize(file_path)
+#     @file_path = file_path
+#     @set_one = []
+#     @set_two = []
+#     @diffs = []
+
+#     parse
+#     sort
+#     match
+#     @sum = diffs.sum
+#   end
+
+#   private
+
+#   attr_reader :file_path
+
+#   def parse
+#     File.foreach(file_path) do |line|
+#       parts = line.split
+#       set_one << parts[0].to_i
+#       set_two << parts[1].to_i
+#     end
+#   end
+
+#   def sort
+#     @set_one.sort!
+#     @set_two.sort!
+#   end
+
+#   def match
+#     @set_one.each_with_index do |item, index|
+#       @diffs << (item - @set_two[index]).abs
+#     end
+#   end
+# end
--- day_one/part_one/test/day_one_part_one_test.rb
+++ day_one/part_one/test/day_one_part_one_test.rb
@@ -4,24 +4,24 @@ require_relative '../../../test_helper'
 require_relative '../lib/day_one_part_one'
 
 class DayOnePartOneTest < Minitest::Test
-  def setup
-    @dopo = DayOnePartOne.new('day_one/part_one/data/test_data.txt')
-  end
-
   def test_can_parse_data
-    assert_equal([1, 2, 3, 3, 3, 4], @dopo.set_one)
-    assert_equal([3, 3, 3, 4, 5, 9], @dopo.set_two)
+    set_one, set_two = DayOnePartOne.parse('day_one/part_one/data/test_data.txt')
+
+    assert_equal([3, 4, 2, 1, 3, 3], set_one)
+    assert_equal([4, 3, 5, 3, 9, 3], set_two)
   end
 
   def test_can_match_data
-    @dopo = DayOnePartOne.new('day_one/part_one/data/test_data.txt')
+    set_one = [3, 4, 2, 1, 3, 3]
+    set_two = [4, 3, 5, 3, 9, 3]
+    diffs = DayOnePartOne.match(set_one, set_two)
 
-    assert_equal([2, 1, 0, 1, 2, 5], @dopo.diffs)
+    assert_equal([2, 1, 0, 1, 2, 5], diffs)
   end
 
-  def test_can_sum
-    @dopo = DayOnePartOne.new('day_one/part_one/data/test_data.txt')
+  def test_can_return_the_correct_result
+    result = DayOnePartOne.run('day_one/part_one/data/test_data.txt')
 
-    assert_equal(11, @dopo.sum)
+    assert_equal(11, result)
   end
 end
