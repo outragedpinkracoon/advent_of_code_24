1bea5f8869168e2670e8ba7ec3ec576b075da85c
Valerie Dryden <outragedpinkracoon@gmail.com>
2024-12-01 21:38:11 +0000

    tidyies up match

--- day_one/part_one/lib/day_one_part_one.rb
+++ day_one/part_one/lib/day_one_part_one.rb
@@ -22,14 +22,13 @@ module DayOnePartOne
   # take two arrays [1,3,2], [4,4,5] sorts them from smallest to biggest
   # and returns the diff between the two numbers [3,1,3]
   def self.match(set_one, set_two)
-    diffs = []
-    sorted_set_one = set_one.sort
     sorted_set_two = set_two.sort
 
-    sorted_set_one.each_with_index do |item, index|
-      diffs << (item - sorted_set_two[index]).abs
+    set_one
+      .sort
+      .each_with_index
+      .with_object([]) do |(item, index), obj|
+      obj << (item - sorted_set_two[index]).abs
     end
-
-    diffs
   end
 end
