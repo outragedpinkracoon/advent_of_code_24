f119425cf3871fa2bd5d0e81b9ef2ce8571f8103
Valerie Dryden <outragedpinkracoon@gmail.com>
2024-12-02 20:36:00 +0000

    checks increasing

--- day_two/part_one/lib/day_two_part_one.rb
+++ day_two/part_one/lib/day_two_part_one.rb
@@ -2,7 +2,35 @@
 
 module DayTwo
   module PartOne
-    def self.run(_file_path)
+    def self.run(file_path)
+      reports = parse(file_path)
+      check_safety(reports)
+    end
+
+    def self.parse(file_path)
+      File.readlines(file_path).map do |line|
+        line.split.map(&:to_i)
+      end
+    end
+
+    def self.check_safety(reports)
+      reports.map do |report|
+        safe?(report)
+      end
+    end
+
+    def self.safe?(report)
+      all_increasing?(report)
+    end
+
+    def self.all_increasing?(report)
+      current_number = report.first
+      report[1..report.count].each do |item|
+        next if current_number < item
+
+        current_number = item
+        return false
+      end
       true
     end
   end
--- day_two/part_one/test/day_two_part_one_test.rb
+++ day_two/part_one/test/day_two_part_one_test.rb
@@ -5,7 +5,18 @@ require_relative '../lib/day_two_part_one'
 
 class DayTwoPartOneTest < Minitest::Test
   def test_can_parse_data
-    result = DayOne::PartTwo.run('day_one/part_one/data/test_data.txt')
+    result = DayTwo::PartOne.parse('day_two/part_one/data/test_data.txt')
+
+    assert_equal([7, 6, 4, 2, 1], result[0])
+    assert_equal(6, result.count)
+  end
+
+  def test_can_check_all_increasing
+    result = DayTwo::PartOne.all_increasing?([7, 6, 4, 2, 1])
+
+    refute(result)
+
+    result = DayTwo::PartOne.all_increasing?([1, 2, 4, 6, 7])
 
     assert(result)
   end
