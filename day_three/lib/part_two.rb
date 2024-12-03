55a2bb6a14f21c0cf7731fe3b10b56a83b1e8dac
Valerie Dryden <outragedpinkracoon@gmail.com>
2024-12-03 16:41:53 +0000

    wip

--- day_three/lib/part_two.rb
+++ day_three/lib/part_two.rb
@@ -2,5 +2,13 @@
 
 module DayThree
   module PartTwo
+    def self.run(file_path)
+      input = File.read(file_path)
+      matches = find_matches(input)
+      DayThree::PartOne.mulitply_results(matches)
+    end
+
+    def matches(input)
+    end
   end
 end
--- day_three/test/part_two_test.rb
+++ day_three/test/part_two_test.rb
@@ -5,4 +5,9 @@ require_relative '../lib/part_two'
 require 'pry-byebug'
 
 class DayThreePartTwoTest < Minitest::Test
+  def test_can_return_correct_result
+    result = DayThree::PartTwo.run('day_three/data/test_data.txt')
+
+    assert_equal(161, result)
+  end
 end
