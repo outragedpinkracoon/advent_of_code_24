18314b861487eba176aa4da8ea0a9771cfcac28e
Valerie Dryden <outragedpinkracoon@gmail.com>
2024-12-03 16:14:39 +0000

    passes day 3 part one

--- day_three/lib/part_one.rb
+++ day_three/lib/part_one.rb
@@ -5,9 +5,22 @@ module DayThree
     def self.run(file_path)
       regex = /mul\(\d{1,3},\d{1,3}\)/
 
-      File.readlines(file_path)[0]
-          .scan(regex)
-          .count
+      input = File.read(file_path)
+      matches = input.scan(regex)
+      mulitply_results(matches)
+    end
+
+    # takes in matches and mulitplies the result together
+    # ['mul(2,3)', 'mul(4,5)'] => 6 + 20 = 26
+    def self.mulitply_results(input)
+      # Extract the pairs of numbers and calculate the products
+      multiples = input.map do |item|
+        # Extract the numbers as a pair using a regex
+        pair = item.scan(/\d+/).map(&:to_i)
+        # Multiply the two numbers
+        pair.reduce(:*)
+      end
+      multiples.sum
     end
   end
 end
--- day_three/test/part_one_test.rb
+++ day_three/test/part_one_test.rb
@@ -5,9 +5,16 @@ require_relative '../lib/part_one'
 require 'pry-byebug'
 
 class DayThreePartOneTest < Minitest::Test
-  def test_can_parse_data
+  def test_can_return_correct_result
     result = DayThree::PartOne.run('day_three/data/test_data.txt')
 
-    assert_equal(4, result)
+    assert_equal(161, result)
+  end
+
+  def test_can_mulitply_the_matches
+    input = ['mul(2,4)', 'mul(5,5)', 'mul(11,8)', 'mul(8,5)']
+    result = DayThree::PartOne.mulitply_results(input)
+
+    assert_equal(161, result)
   end
 end
