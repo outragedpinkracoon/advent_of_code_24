3d29eeac91c8032b85f3c0f426f02eaaa31f7e05
Valerie Dryden <outragedpinkracoon@gmail.com>
2024-12-04 21:41:07 +0000

    finds diagonals

--- day_four/lib/part_one.rb
+++ day_four/lib/part_one.rb
@@ -2,5 +2,52 @@
 
 module DayFour
   module PartOne
+    def self.run(file_path)
+      input = File.readlines(file_path, chomp: true)
+      grid = map_to_coords(input)
+      diagonals = find_diagonals(grid)
+      count = 0
+      diagonals.each do |diagonal_list|
+        count += find_word_count(diagonal_list, 'XM')
+      end
+      count
+    end
+
+    def self.find_diagonals(grid, limit:)
+      # Grouping for top-left to bottom-right (↘) and bottom-right to top-left (↖)
+      top_left_to_bottom_right = grid.group_by { |(_, row, col)| row - col }
+
+      # Grouping for top-right to bottom-left (↙) and bottom-left to top-right (↗)
+      top_right_to_bottom_left = grid.group_by { |(_, row, col)| row + col }
+
+      valid_diagonals_tl_br = top_left_to_bottom_right.values.select { |diag| diag.size >= limit }
+      valid_diagonals_tr_bl = top_right_to_bottom_left.values.select { |diag| diag.size >= limit }
+
+      valid_diagonals_tl_br + valid_diagonals_tr_bl
+    end
+
+    # takes an array of co-ords and finds how many times the word is in it
+    # input = [[["X", 1, 1], ["M", 2, 2], ["A", 3, 3], ["S", 4, 4]]]
+    def self.find_word_count(coord_list, word)
+      matches = []
+      word_length = word.length
+      (0..(coord_list.size - word_length)).each do |start_index|
+        segment = coord_list[start_index, word_length]
+        matches << segment if segment == word.chars
+      end
+      matches.count
+    end
+
+    # takes in an array like ["XZZZ", "ZMZZ", "ZZAZ", "ZZZS"] and maps each item
+    # to it's relevant co-ordinate in a matrix e.g. [["X", 1, 1], "Z", 1, 2]
+    def self.map_to_coords(input)
+      row_number = 1
+      input.each_with_object([]) do |row, obj|
+        row.each_char.with_index do |letter, index|
+          obj << [letter, row_number, index + 1]
+        end
+        row_number += 1
+      end
+    end
   end
 end
--- day_four/test/part_one_test.rb
+++ day_four/test/part_one_test.rb
@@ -5,4 +5,42 @@ require_relative '../lib/part_one'
 require 'pry-byebug'
 
 class DayFourPartOneTest < Minitest::Test
+  def test_can_return_correct_result
+    input = %w[XZZZ ZMZZ ZZAZ ZZZS]
+    result = DayFour::PartOne.map_to_coords(input)
+
+    expected = [
+      ['X', 1, 1], ['Z', 1, 2], ['Z', 1, 3], ['Z', 1, 4],
+      ['Z', 2, 1], ['M', 2, 2], ['Z', 2, 3], ['Z', 2, 4],
+      ['Z', 3, 1], ['Z', 3, 2], ['A', 3, 3], ['Z', 3, 4],
+      ['Z', 4, 1], ['Z', 4, 2], ['Z', 4, 3], ['S', 4, 4]
+    ]
+
+    assert_equal(expected, result)
+  end
+
+  def test_can_make_groupings
+    input = [
+      ['X', 1, 1], ['B', 1, 2], ['C', 1, 3], ['M', 1, 4],
+      ['E', 2, 1], ['M', 2, 2], ['X', 2, 3], ['G', 2, 4],
+      ['H', 3, 1], ['I', 3, 2], ['X', 3, 3], ['J', 3, 4],
+      ['D', 4, 1], ['K', 4, 2], ['J', 4, 3], ['M', 4, 4]
+    ]
+
+    groupings = DayFour::PartOne.find_diagonals(input, limit: 2)
+
+    expected = [
+      [['X', 1, 1], ['M', 2, 2], ['X', 3, 3], ['M', 4, 4]],
+      [['B', 1, 2], ['X', 2, 3], ['J', 3, 4]],
+      [['C', 1, 3], ['G', 2, 4]],
+      [['E', 2, 1], ['I', 3, 2], ['J', 4, 3]],
+      [['H', 3, 1], ['K', 4, 2]], [['B', 1, 2], ['E', 2, 1]],
+      [['C', 1, 3], ['M', 2, 2], ['H', 3, 1]],
+      [['M', 1, 4], ['X', 2, 3], ['I', 3, 2], ['D', 4, 1]],
+      [['G', 2, 4], ['X', 3, 3], ['K', 4, 2]],
+      [['J', 3, 4], ['J', 4, 3]]
+    ]
+
+    assert_equal(expected, groupings)
+  end
 end
