3fd58449f85368411606872557cff01fb9fc149b
Valerie Dryden <outragedpinkracoon@gmail.com>
2024-12-06 21:32:38 +0000

    adds day 5 plumbing

---
+++ day_five/lib/part_one.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module DayFive
+  module PartOne
+  end
+end
---
+++ day_five/test/part_two_test.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative '../../test_helper'
+require_relative '../lib/part_two'
+require 'pry-byebug'
+
+class DayFivePartTwoTest < Minitest::Test
+end
