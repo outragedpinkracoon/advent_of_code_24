d86134ec532cb019b6d3fe4629e8985f568a9a69
Valerie Dryden <outragedpinkracoon@gmail.com>
2024-12-06 21:58:24 +0000

    parses file first step

--- day_five/lib/part_one.rb
+++ day_five/lib/part_one.rb
@@ -2,5 +2,22 @@
 
 module DayFive
   module PartOne
+    def self.run(file_path)
+      parse_sections(file_path)
+    end
+
+    def self.parse_sections(file_path)
+      file_content = File.read(file_path)
+
+      # Split the content into two parts based on the empty line
+      parts = file_content.split("\n\n")
+
+      # Access each part
+      rules = parts[0]
+      updates = parts[1]
+      [rules, updates]
+    end
+
+    def generate_rules(ruleset); end
   end
 end
--- day_five/test/part_one_test.rb
+++ day_five/test/part_one_test.rb
@@ -5,4 +5,28 @@ require_relative '../lib/part_one'
 require 'pry-byebug'
 
 class DayFivePartOneTest < Minitest::Test
+  def test_returns_correct_answer_for_input_file
+    result = DayFive::PartOne.run('day_five/data/small_test_data.txt')
+    expected_part_one = <<~TEXT.chomp
+      75|47
+      75|61
+      75|53
+      75|29
+      75|47
+      47|61
+      47|53
+      47|29
+      61|53
+      61|29
+      53|29
+    TEXT
+
+    expected_part_two = <<~TEXT.chomp
+      75,47,61,53,29
+      75,97,47,61,53
+    TEXT
+
+    assert_equal(expected_part_one, result[0])
+    assert_equal(expected_part_two, result[1])
+  end
 end
